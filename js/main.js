4ad479785ccbd94fbe9e028f4a80de24dcaa5beb
TuranovDanil <turanoff.danil@yandex.ru>
2023-02-04 18:53:56 +0700

    сделал 3 столобца

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let eventBus = new Vue();
 
-Vue.component('left-list', {
+Vue.component('list', {
     props: {
         note_data: {
             type: Object,
@@ -53,8 +53,7 @@ Vue.component('left-list', {
 
     },
     template: `
-    <div class="left-list">
-        <div class="column column1" v-if="note_data.completedNum <= 50">
+    <div class="list">
             <div class="create_task">
                 <h3 class="title_block">{{note_data.noteTitle}}</h3>
                 <button @click="delNote()">X</button>
@@ -63,15 +62,14 @@ Vue.component('left-list', {
                 <div v-for="(element, elementId) in note_data.tasks" :key="elementId">
                     <div class="set_task">
                         <p class="title_task">{{element.taskTitle}}</p>
-                        <input @click="checkbox(elementId)" type="checkbox" v-model="element.completed">
+                        <input @click="checkbox(elementId)" type="checkbox" v-model="element.completed" :class="{none: note_data.completedNum === 100}">
                     </div>
                 </div>
-                <div class="add_task">                  
+                <div class="add_task" :class="{none: note_data.tasks.length >= 5}">                  
                     <div class="add_task_input">
-                        <input type="text" @keyup.enter="addTask" v-model="taskTitle" placeholder="Задача">
+                        <input required type="text" @keyup.enter="addTask" v-model="taskTitle" placeholder="Задача">
                     </div>
                     <button @click="addTask">Добавить</button>
-                </div>
             </div>
         </div>
     </div>
@@ -79,114 +77,114 @@ Vue.component('left-list', {
     `,
 })
 
-Vue.component('center-list', {
-    props: {
-        note_data: {
-            type: Object,
-            default() {
-                return {}
-            }
-        },
-        notes: {
-            type: Array,
-            default() {
-                return {}
-            }
-        }
-    },
-    data() {
-        return {
-            taskTitle: null,
-            task: [],
-        }
-
-    },
-    methods: {
-        delNote() {
-            this.$emit('del_note')
-        },
-        addTask() {
-            if (this.taskTitle) {
-                this.note_data.tasks.push({
-                    taskTitle: this.taskTitle,
-                    completed: false,
-                });
-                this.taskTitle = null;
-                localStorage.todo = JSON.stringify(this.notes)
-            }
-        },
-        checkbox(id){
-            this.note_data.tasks[id].completed = !this.note_data.tasks[id].completed;
-            let counterCompleted = 0;
-            let counterNotCompleted = 0;
-            for (let el of this.note_data.tasks) {
-                if (el.completed) {
-                    counterCompleted++;
-                } else {
-                    counterNotCompleted++;
-                }
-            }
-            this.note_data.completedNum = (counterCompleted / (counterCompleted + counterNotCompleted)) * 100;
-            localStorage.todo = JSON.stringify(this.notes);
-        },
-    },
-    template: `
-    <div class="center-list">
-        <div class="column column2" v-if="note_data.completedNum > 50">
-            <div class="create_task">
-                <h3 class="title_block">{{note_data.noteTitle}}</h3>
-                <button @click="delNote()">X</button>
-            </div>
-            <div class="task">
-                <div v-for="(element, elementId) in note_data.tasks" :key="elementId">
-                    <div class="set_task">
-                        <p class="title_task">{{element.taskTitle}}</p>
-                        <input @click="checkbox(elementId)" type="checkbox" v-model="element.completed">
-                    </div>
-                </div>
-                <div class="add_task">                  
-                    <div class="add_task_input">
-                        <input type="text" @keyup.enter="addTask" v-model="taskTitle" placeholder="Задача">
-                    </div>
-                    <button @click="addTask">Добавить</button>
-                </div>
-            </div>
-        </div>
-    </div>
-    `,
-})
-
-Vue.component('right-list', {
-    template: `
-    <div class="right-list">
-        <div class="column column3">
-            <div class="create_task">
-                <h3 class="title_block">{{note_data.noteTitle}}</h3>
-                <button @click="delNote()">X</button>
-            </div>
-            <div class="task">
-                <div v-for="(element, elementId) in note_data.tasks" :key="elementId">
-                    <div class="set_task">
-                        <p class="title_task">{{element.taskTitle}}</p>
-                        <input @click="checkbox(elementId)" type="checkbox" v-model="element.completed">
-                    </div>
-                </div>
-                <div class="add_task">                  
-                    <div class="add_task_input">
-                        <input type="text" @keyup.enter="addTask" v-model="taskTitle" placeholder="Задача">
-                    </div>
-                    <button @click="addTask">Добавить</button>
-                </div>
-            </div>
-        </div>
-    </div>
-    `,
-    data() {
-        return {};
-    },
-    methods: {},
-    computed: {}
-})
+// Vue.component('center-list', {
+//     props: {
+//         note_data: {
+//             type: Object,
+//             default() {
+//                 return {}
+//             }
+//         },
+//         notes: {
+//             type: Array,
+//             default() {
+//                 return {}
+//             }
+//         }
+//     },
+//     data() {
+//         return {
+//             taskTitle: null,
+//             task: [],
+//         }
+//
+//     },
+//     methods: {
+//         delNote() {
+//             this.$emit('del_note')
+//         },
+//         addTask() {
+//             if (this.taskTitle) {
+//                 this.note_data.tasks.push({
+//                     taskTitle: this.taskTitle,
+//                     completed: false,
+//                 });
+//                 this.taskTitle = null;
+//                 localStorage.todo = JSON.stringify(this.notes)
+//             }
+//         },
+//         checkbox(id){
+//             this.note_data.tasks[id].completed = !this.note_data.tasks[id].completed;
+//             let counterCompleted = 0;
+//             let counterNotCompleted = 0;
+//             for (let el of this.note_data.tasks) {
+//                 if (el.completed) {
+//                     counterCompleted++;
+//                 } else {
+//                     counterNotCompleted++;
+//                 }
+//             }
+//             this.note_data.completedNum = (counterCompleted / (counterCompleted + counterNotCompleted)) * 100;
+//             localStorage.todo = JSON.stringify(this.notes);
+//         },
+//     },
+//     template: `
+//     <div class="center-list">
+//         <div class="column column2" v-if="note_data.completedNum > 50">
+//             <div class="create_task">
+//                 <h3 class="title_block">{{note_data.noteTitle}}</h3>
+//                 <button @click="delNote()">X</button>
+//             </div>
+//             <div class="task">
+//                 <div v-for="(element, elementId) in note_data.tasks" :key="elementId">
+//                     <div class="set_task">
+//                         <p class="title_task">{{element.taskTitle}}</p>
+//                         <input @click="checkbox(elementId)" type="checkbox" v-model="element.completed">
+//                     </div>
+//                 </div>
+//                 <div class="add_task">
+//                     <div class="add_task_input">
+//                         <input type="text" @keyup.enter="addTask" v-model="taskTitle" placeholder="Задача">
+//                     </div>
+//                     <button @click="addTask">Добавить</button>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+//     `,
+// })
+//
+// Vue.component('right-list', {
+//     template: `
+//     <div class="right-list">
+//         <div class="column column3">
+//             <div class="create_task">
+//                 <h3 class="title_block">{{note_data.noteTitle}}</h3>
+//                 <button @click="delNote()">X</button>
+//             </div>
+//             <div class="task">
+//                 <div v-for="(element, elementId) in note_data.tasks" :key="elementId">
+//                     <div class="set_task">
+//                         <p class="title_task">{{element.taskTitle}}</p>
+//                         <input @click="checkbox(elementId)" type="checkbox" v-model="element.completed">
+//                     </div>
+//                 </div>
+//                 <div class="add_task">
+//                     <div class="add_task_input">
+//                         <input type="text" @keyup.enter="addTask" v-model="taskTitle" placeholder="Задача">
+//                     </div>
+//                     <button @click="addTask">Добавить</button>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+//     `,
+//     data() {
+//         return {};
+//     },
+//     methods: {},
+//     computed: {}
+// })
 
 
 let app = new Vue({
@@ -224,6 +222,6 @@ let app = new Vue({
         deleteNote(id) {
             this.notes.splice(id, 1);
             localStorage.todo = JSON.stringify(this.notes);
-        }
+        },
     },
 })
