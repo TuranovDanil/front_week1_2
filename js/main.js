fd5ee90ab682d52b1e98904005133ce2c1f7ce01
TuranovDanil <turanoff.danil@yandex.ru>
2023-02-04 14:14:58 +0700

    переделал концепцию, сделал добавление заметки и добавление пунктов

--- js/main.js
+++ js/main.js
@@ -1,148 +1,137 @@
 let eventBus = new Vue();
 
-Vue.component('container', {
-    template: `
-    <div class="container">
-        <left-list></left-list>
-        <center-list></center-list>
-        <right-list></right-list>
-    </div>
-    `,
+Vue.component('left-list', {
+    props: {
+        note_data: {
+            type: Object,
+            default() {
+                return {}
+            }
+        },
+        notes: {
+            type: Array,
+            default() {
+                return {}
+            }
+        }
+    },
     data() {
-        return {};
+        return {
+            taskTitle: null,
+            task: [],
+            completed: false
+        }
+
     },
     methods: {
+        addTask() {
+            if (this.taskTitle){
+                this.note_data.tasks.push({
+                    taskTitle: this.taskTitle,
+                    completed: false,
+                });
+                this.taskTitle = null;
+                localStorage.todo = JSON.stringify(this.notes)
+            }
+        },
+        checkbox(id){
+            this.note_data.tasks[id].completed = !this.note_data.tasks[id].completed;
+            localStorage.todo = JSON.stringify(this.notes);
+        }
 
     },
-    computed: {
-
-    }
-
-})
-
-Vue.component('left-list', {
+    computed: {},
     template: `
-    <div class="left-list">
-        <div class="column column1">
-            <h2>1</h2>
-            <div>
-            <h3>Название</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-            <h3>Название</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-            <h3>Название</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
+    <div class="center-list">
+        <div class="column column2">
+            <div class="create_task">
+                <h3 class="title_block">{{note_data.noteTitle}}</h3>
+<!--                            <button @click="noteDelete()">X</button>-->
+            </div>
+            <div class="task">
+                <div v-for="(element, elementId) in note_data.tasks" :key="elementId">
+                    <div class="set_task">
+                        <p class="title_task">{{element.taskTitle}}</p>
+                        <p >{{element.completed}}</p>
+                        <input @click="checkbox(elementId)" type="checkbox" v-model="element.completed">
+                    </div>
+                </div>
+                <div class="add_task">
+                    <div class="add_task_input">
+                        <input type="text" @keyup.enter="addTask()" v-model="note_data.tasks.taskTitle" placeholder="Задача">
+                    </div>
+                    <button @click="addTask()">Добавить</button>
+                </div>
             </div>
         </div>
     </div>
+
     `,
     data() {
         return {};
     },
-    methods: {
 
-    },
-    computed: {
-
-    }
 })
 
 Vue.component('center-list', {
     template: `
     <div class="center-list">
         <div class="column column2">
-            <h2>2</h2>
-            <div>
-            <h3>Название</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-            <h3>Название</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-            <h3>Название</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-            </div>        
+
         </div>
     </div>
     `,
     data() {
         return {};
     },
-    methods: {
-
-    },
-    computed: {
-
-    }
+    methods: {},
+    computed: {}
 })
 
 Vue.component('right-list', {
     template: `
     <div class="right-list">
         <div class="column column3">
-            <h2>3</h2>
-            <div>
-            <h3>Название</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-            <h3>Название</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-            <h3>Название</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-            </div>
+
         </div>
     </div>
     `,
     data() {
         return {};
     },
-    methods: {
-
-    },
-    computed: {
-
-    }
+    methods: {},
+    computed: {}
 })
+
 
 let app = new Vue({
     el: '#app',
     data: {
-
+        notes: [],
+        noteTitle: null,
+        todos: []
+    },
+    mounted() {
+        if (localStorage.todo) {
+            this.notes = JSON.parse(localStorage.todo)
+        }
     },
     methods: {
+        addInTodos() {
+            this.todos.push({
+                notes: this.notes
+            })
 
-    }
+        },
+        createNote() {
+            if(this.noteTitle){
+                this.notes.push({
+                    noteTitle: this.noteTitle,
+                    tasks: [],
+                });
+                this.noteTitle = null;
+                localStorage.todo = JSON.stringify(this.notes)
+            }
+        }
+    },
 })
